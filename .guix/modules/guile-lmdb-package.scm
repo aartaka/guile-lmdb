3a1e51ba942012d0b7f2698da4bb635a580490ed
Artyom Bologov <aartaka@protonmail.com>
2024-01-15 17:35:10 +0400

    .guix/modules/guile-lmdb-package: define-public the package.

--- .guix/modules/guile-lmdb-package.scm
+++ .guix/modules/guile-lmdb-package.scm
@@ -8,18 +8,19 @@
  #:use-module (guix git-download)
  #:use-module ((guix licenses) #:prefix license:))
 
-(package
- (name "guile-lmdb")
- (version "0.0.1")
- (source (local-file (dirname (current-filename))
-                     #:recursive? #t
-                     #:select? (git-predicate (dirname (current-source-directory)))))
- (build-system guile-build-system)
- (native-inputs (list guile-3.0))
- (inputs (list guile-3.0 lmdb))
- (home-page "https://github.com/aartaka/guile-lmdb")
- (synopsis "Bindings for LMDB (Lightning Memory-Mapped Database) in Guile.")
- (description "Scheme wrapper around liblmdb.so.
+(define-public guile-lmdb
+  (package
+   (name "guile-lmdb")
+   (version "0.0.1")
+   (source (local-file (dirname (current-filename))
+                       #:recursive? #t
+                       #:select? (git-predicate (dirname (current-source-directory)))))
+   (build-system guile-build-system)
+   (native-inputs (list guile-3.0))
+   (inputs (list guile-3.0 lmdb))
+   (home-page "https://github.com/aartaka/guile-lmdb")
+   (synopsis "Bindings for LMDB (Lightning Memory-Mapped Database) in Guile.")
+   (description "Scheme wrapper around liblmdb.so.
 Most name are the same as LMDB ones, except for prefix absence.
 Several conveniences are added on top:
 @itemize
@@ -28,4 +29,6 @@ Several conveniences are added on top:
 @item @code{val} and @code{stat} types.
 @item Error signaling instead of integer return values.
 @end itemize\n")
- (license license:bsd-2))
+   (license license:bsd-2)))
+
+guile-lmdb
